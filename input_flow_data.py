273cc29665adc0f048d32e4b27103c70fd3f9ba9
cleber ximenes <cleber.ximenes@gmail.com>
2018-03-27 16:03:52 -0300

    retirado daqui peso molecular (mw), densidade do líquido (liquid_density), etc...

--- input_flow_data.py
+++ input_flow_data.py
@@ -15,8 +15,5 @@ def input_flow_data_function():
     visc_f = 279e-6                         #kg/(m s) @ [Tsat(P_amb)] - pg 529 - Ghiaasiaan
     gas_mass_flow = 0.                      #[kg/s]
     liquid_mass_flow = 0.2                  #[kg/s]
-    molar_mass = 18.                        #[kg/kmol]
-    liquid_density = np.power(0.00104,-1)   #[kg/m3]
 
-    return (pressure, temperature, gas_mass_flow, liquid_mass_flow, 
-    molar_mass, visc_g, visc_f, liquid_density)
+    return (pressure, temperature, gas_mass_flow, liquid_mass_flow, visc_g, visc_f)
