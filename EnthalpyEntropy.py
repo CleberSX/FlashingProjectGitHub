cad9e96ff91be7d768ff2a05091356ace63dde48
cleber ximenes <cleber.ximenes@gmail.com>
2018-03-28 19:38:10 -0300

    transformei em classe

--- EnthalpyEntropy.py
+++ EnthalpyEntropy.py
@@ -96,7 +96,6 @@ z, z_mass = Tools_Convert.frac_input(MM, zin, base)
 =========================================================================================================
 '''
 TR = 273.15
-pR, y_sat, Sy_sat, counter = bubble_obj(TR, z)
 hR_mass = 200e3
 hR = hR_mass * prop_obj.calculate_weight_molar_mixture(MM, z, 'saturated_liquid')
 sR_mass = 1000.
@@ -110,63 +109,72 @@ is into subcooled region or ELV. To do this, first the bubble pressure at intere
 by method bubble_obj(). It has been built this way, inside a function, to be useful when building the flow code.  
 =========================================================================================================
 '''
+class HSFv:
+      '''THIS CLASS CALCULATE ENTHALPY AND ENTROPY; ALSO DETERMINE QUALITY WHEN TWO-PHASE OCCURS'''
+      def __init__(self, pC, TR, Tc, AcF, Cp, MM, hR, sR, printResults=True):
+            self.pC, self.TR, self.Tc = pC, TR, Tc
+            self.AcF, self.Cp, self.MM, self.hR, self.sR = AcF, Cp, MM, hR, sR
+            self.printResults = printResults
 
-def calculate_enthalpy_entropy(p, pR, pC, T, TR, Tc, AcF, Cp, z, MM, hR, sR, printResults=False):
-      pB, y_sat, Sy, counter = bubble_obj(T, z)
-      if printResults:
-            print('You\'re running pressure = %.3e [Pa] and temperature = %.2f C' % (p, (T - 273.15)))
-            print('For this temperature the bubble pressure is: pB = %.3e [Pa] at T = (%.2f C)' % (pB, (T - 273.15)))
-            print('\n---------------------------------------------------------)')
-      if (p >= pB):
-            F_V = 0.0
-            H_subcooled = prop_obj.calculate_enthalpy(TR, T, pR, p, z, z, hR, Cp, 'liquid')
-            M_L = prop_obj.calculate_weight_molar_mixture(MM, z, 'liquid')
-            H_subcooled_mass = H_subcooled * np.reciprocal(M_L)
-            S_subcooled = prop_obj.calculate_entropy(TR, T, pR, p, z, z, sR, Cp, 'liquid')
-            S_subcooled_mass = S_subcooled * np.reciprocal(M_L)
-            h, s = H_subcooled_mass, S_subcooled_mass
+      def __call__(self, p, pR, T, z):
+            pC, TR, Tc = self.pC, self.TR, self.Tc
+            AcF, Cp, MM, hR, sR = self.AcF, self.Cp, self.MM, self.hR, self.sR 
+            printResults = self.printResults
+            pB, y_sat, Sy, counter = bubble_obj(T, z)
             if printResults:
-                  print('--> This @(p,T) falls into single phase region')
-                  print('\nSubcooled liquid with h = %.3e [J/ kg]' % H_subcooled_mass)
-                  print('\nSubcooled liquid with s = %.3e [J/(kg K)]' % S_subcooled_mass)
-      else:
-            if printResults:
-                  print('This @(p,T) falls into two phase region')
-            F_V, is_stable, K_values_newton, initial_K_values = \
-                FlashAlgorithm_main.getting_the_results_from_FlashAlgorithm_main(p, T, pC, Tc, AcF, z)
-            x = z / (F_V * (K_values_newton - 1.) + 1.)
-            y = K_values_newton * x
-            x_mass = Tools_Convert.convert_molarfrac_TO_massfrac(MM, x)
-            y_mass = Tools_Convert.convert_molarfrac_TO_massfrac(MM, y)
-            H_sat_vapor = prop_obj.calculate_enthalpy(TR, T, pR, p, y, z, hR, Cp, 'saturated_vapor')
-            M_V = prop_obj.calculate_weight_molar_mixture(MM, y, 'saturated_vapor')
-            H_sat_vapor_mass = H_sat_vapor * np.reciprocal(M_V)
-            H_sat_liquid = prop_obj.calculate_enthalpy(TR, T, pR, p, x, z, hR, Cp,'saturated_liquid')
-            M_L = prop_obj.calculate_weight_molar_mixture(MM, x, 'saturated_liquid')
-            H_sat_liquid_mass = H_sat_liquid * np.reciprocal(M_L)
-            enthalpy_mixture = (1. - F_V) * H_sat_liquid + F_V * H_sat_vapor
-            F_V_mass = np.reciprocal((M_L / M_V) * (1 / F_V - 1.) + 1.)
-            enthalpy_mixture_mass = (1. - F_V_mass) * H_sat_liquid_mass + F_V_mass * H_sat_vapor_mass
-            if printResults:
-                  print('The mixture\'s state is ELV with a quality = %.3f [molar base]' % F_V)
-                  print('The mixture\'s state is ELV with a quality = %.3f [mass base]' % F_V_mass)
-                  print('x [molar base] = ', x)
-                  print('y [molar base] = ', y)
-                  print('x_mass [mass base] = ', x_mass)
-                  print('y_mass [mass base] = ', y_mass)
-                  print('\n======\nThe mixture liquid/vapor with h = %.3e [J/ kmol]' % enthalpy_mixture)
-                  print('The mixture liquid/vapor with h_mass = %.3e [J/ kg] {mass base}' % enthalpy_mixture_mass)
-            S_sat_vapor = prop_obj.calculate_entropy(TR, T, pR, p, y, z, sR, Cp, 'vapor')
-            S_sat_vapor_mass = S_sat_vapor * np.reciprocal(M_V)
-            S_sat_liquid = prop_obj.calculate_entropy(TR, T, pR, p, x, z, sR, Cp, 'liquid')
-            S_sat_liquid_mass = S_sat_liquid * np.reciprocal(M_L)
-            entropy_mixture = (1. - F_V) * S_sat_liquid + F_V * S_sat_vapor
-            entropy_mixture_mass = (1. - F_V_mass) * S_sat_liquid_mass + F_V_mass * S_sat_vapor_mass
-            h, s = enthalpy_mixture_mass, entropy_mixture_mass
-            if printResults:
-                  print('\n======\nThe mixture liquid/vapor with s = %.3e [J/(kmol K)]' % entropy_mixture)
-                  print('The mixture liquid/vapor with s_mass = %.3e [J/(kg K)] {mass base}' % entropy_mixture_mass)
-      return F_V, h, s
+                  print('You\'re running pressure = %.3e [Pa] and temperature = %.2f C' % (p, (T - 273.15)))
+                  print('For this temperature the bubble pressure is: pB = %.3e [Pa] at T = (%.2f C)' % (pB, (T - 273.15)))
+                  print('\n---------------------------------------------------------)')
+            if (p >= pB):
+                  F_V = 0.0
+                  H_subcooled = prop_obj.calculate_enthalpy(TR, T, pR, p, z, z, hR, Cp, 'liquid')
+                  M_L = prop_obj.calculate_weight_molar_mixture(MM, z, 'liquid')
+                  H_subcooled_mass = H_subcooled * np.reciprocal(M_L)
+                  S_subcooled = prop_obj.calculate_entropy(TR, T, pR, p, z, z, sR, Cp, 'liquid')
+                  S_subcooled_mass = S_subcooled * np.reciprocal(M_L)
+                  h, s = H_subcooled_mass, S_subcooled_mass
+                  if printResults:
+                        print('--> This @(p,T) falls into single phase region')
+                        print('\nSubcooled liquid with h = %.3e [J/ kg]' % H_subcooled_mass)
+                        print('\nSubcooled liquid with s = %.3e [J/(kg K)]' % S_subcooled_mass)
+            else:
+                  if printResults:
+                        print('This @(p,T) falls into two phase region')
+                  F_V, is_stable, K_values_newton, initial_K_values = \
+                  FlashAlgorithm_main.getting_the_results_from_FlashAlgorithm_main(p, T, pC, Tc, AcF, z)
+                  x = z / (F_V * (K_values_newton - 1.) + 1.)
+                  y = K_values_newton * x
+                  x_mass = Tools_Convert.convert_molarfrac_TO_massfrac(MM, x)
+                  y_mass = Tools_Convert.convert_molarfrac_TO_massfrac(MM, y)
+                  H_sat_vapor = prop_obj.calculate_enthalpy(TR, T, pR, p, y, z, hR, Cp, 'saturated_vapor')
+                  M_V = prop_obj.calculate_weight_molar_mixture(MM, y, 'saturated_vapor')
+                  H_sat_vapor_mass = H_sat_vapor * np.reciprocal(M_V)
+                  H_sat_liquid = prop_obj.calculate_enthalpy(TR, T, pR, p, x, z, hR, Cp,'saturated_liquid')
+                  M_L = prop_obj.calculate_weight_molar_mixture(MM, x, 'saturated_liquid')
+                  H_sat_liquid_mass = H_sat_liquid * np.reciprocal(M_L)
+                  enthalpy_mixture = (1. - F_V) * H_sat_liquid + F_V * H_sat_vapor
+                  F_V_mass = np.reciprocal((M_L / M_V) * (1 / F_V - 1.) + 1.)
+                  enthalpy_mixture_mass = (1. - F_V_mass) * H_sat_liquid_mass + F_V_mass * H_sat_vapor_mass
+                  if printResults:
+                        print('The mixture\'s state is ELV with a quality = %.3f [molar base]' % F_V)
+                        print('The mixture\'s state is ELV with a quality = %.3f [mass base]' % F_V_mass)
+                        print('x [molar base] = ', x)
+                        print('y [molar base] = ', y)
+                        print('x_mass [mass base] = ', x_mass)
+                        print('y_mass [mass base] = ', y_mass)
+                        print('\n======\nThe mixture liquid/vapor with h = %.3e [J/ kmol]' % enthalpy_mixture)
+                        print('The mixture liquid/vapor with h_mass = %.3e [J/ kg] {mass base}' % enthalpy_mixture_mass)
+                  S_sat_vapor = prop_obj.calculate_entropy(TR, T, pR, p, y, z, sR, Cp, 'vapor')
+                  S_sat_vapor_mass = S_sat_vapor * np.reciprocal(M_V)
+                  S_sat_liquid = prop_obj.calculate_entropy(TR, T, pR, p, x, z, sR, Cp, 'liquid')
+                  S_sat_liquid_mass = S_sat_liquid * np.reciprocal(M_L)
+                  entropy_mixture = (1. - F_V) * S_sat_liquid + F_V * S_sat_vapor
+                  entropy_mixture_mass = (1. - F_V_mass) * S_sat_liquid_mass + F_V_mass * S_sat_vapor_mass
+                  h, s = enthalpy_mixture_mass, entropy_mixture_mass
+                  if printResults:
+                        print('\n======\nThe mixture liquid/vapor with s = %.3e [J/(kmol K)]' % entropy_mixture)
+                        print('The mixture liquid/vapor with s_mass = %.3e [J/(kg K)] {mass base}' % entropy_mixture_mass)
+            return F_V, h, s
 
 
 
@@ -174,9 +182,14 @@ def calculate_enthalpy_entropy(p, pR, pC, T, TR, Tc, AcF, Cp, z, MM, hR, sR, pri
 
 '''
 =========================================================================================================
-executing ...  calculate_enthalpy_entropy(p, pR, pC, T, TR, Tc, AcF, Cp, z, MM, hR, sR, printResults=False)
+executing ...  
+1st: creating obj -- instantiating the class
+2nd: executing bubble_obj to obtain TR
+3rd: finaly executing the object for the specific variables (p, pR, T, z)
 =========================================================================================================
 '''
+hsfv_obj = HSFv(pC, TR, Tc, AcF, Cp, MM, hR, sR)
+pR, y_sat, Sy_sat, counter = bubble_obj(TR, z)
+F_V, h, s = hsfv_obj(p, pR, T, z)
 
-F_V, h, s = calculate_enthalpy_entropy(p, pR, pC, T, TR, Tc, AcF, Cp, z, MM, hR, sR, True)
 
