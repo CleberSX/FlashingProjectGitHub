0b17497cb02cea782d45586fb841296055be60a6
cleber ximenes <cleber.ximenes@gmail.com>
2018-04-10 20:08:09 -0300

    deletado viscF e inserido no lugar viscR e viscO...esses dados são necessários para o cálculo do viscL com o uso de um método presente na FlowTools_class

--- input_flow_data.py
+++ input_flow_data.py
@@ -2,17 +2,24 @@ import numpy as np
 import sys
 
 def input_flow_data_function():
+    '''
+    p_e: pipe's entrance pressure [Pa]
+    T_e: pipe's entrance temperature  [K]  
+    mdotL_e: pipe's entrance liquid mass mass ratio [kg/s]
+    viscG: saturated gas's dynamic viscosity [kg/(m.s)]
+    viscO: lubricant oil's dynamic viscosity [kg/(m.s)]
+    viscR: refrigerant's dynamic viscosity [kg/(m.s)]   
+    '''
     this_function_name = sys._getframe().f_code.co_name
     print('\n---\n DATA FLOW ', this_function_name)
     print('\n---\n')
-    '''
-    THESE DATA FLOW HAVE BEEN GOTTEN FROM:_______     
-    '''
+
     room_pressure = 1.e5                    # -- pressao ambiente [Pa]
     temperature = 300.0                     # -- temperatura de entrada [K]
     pressure = 15. * room_pressure          # -- pressão entrada duto [Pa]
-    viscG = 12.02e-6                        # -- (@ satureted gas) [kg/(m s)]
-    viscF = 279e-6                          # -- (@ satureted liquid) [kg/(m s)]
-    subcooled_liquid_mass_flow = 0.2        # -- (subcooled liquid) [kg/(m s)]
+    viscG = 12.02e-6                        # -- (@ saturated gas) [kg/(m s)]
+    viscR = 279e-6                          # -- (@ saturated liquid refrigerant's dynamic viscosity ) [kg/(m s)]
+    viscO =  279e2                               # -- (@ oil's dynamic viscosity ) [kg/(m s)]
+    subcooled_liquid_mass_flow = 0.2        # -- (mass ratio subcooled liquid) [kg/s]
 
-    return (pressure, temperature, subcooled_liquid_mass_flow, viscG, viscF)
+    return (pressure, temperature, subcooled_liquid_mass_flow, viscG, viscR, viscO)
