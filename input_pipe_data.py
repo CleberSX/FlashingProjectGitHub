c84f2167fcfef8c62450068e2919214e2738bf72
cleber ximenes <cleber.ximenes@gmail.com>
2018-04-10 20:08:51 -0300

    alteração de valor comprimento duto apenas

--- input_pipe_data.py
+++ input_pipe_data.py
@@ -9,7 +9,7 @@ def input_pipe_data_function():
     THESE DATA PIPE HAVE BEEN GOTTEN FROM:_______     
     '''
     diameter = (4. / 5) * 25.4 / 1000    #[m] convert {in} to {m}
-    lenght = 1.                    #[m]
+    lenght = 1.0                    #[m]
     rugosity = 1.2e-3                 #[m]               
 
     return (diameter, lenght, rugosity)
