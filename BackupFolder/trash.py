da87418cc64c17ac33595b24a0cb70b19709c35b
cleber ximenes <cleber.ximenes@gmail.com>
2018-05-03 21:55:13 -0300

    principal alteracao foi fazer Area(z) apenas e inserir uma funcao que faz derivada numerica dentro da ODESystem

--- BackupFolder/trash.py
+++ BackupFolder/trash.py
@@ -21,6 +21,12 @@ densL = (1. / (2 * Dp)) * (Gt / Kventuri) ** 2
 print(densL)
 
 
+'''
+=========================
+BRINCANDO COM CLASS
+========================
+ '''
+
 class MyParentClass():
     def __init__(self, x, y):
         self.x, self.y = x, y
@@ -49,4 +55,63 @@ obj = SubClass(10., 20., 1., 2., 3.)
 
 
 print(obj.somaElevadop(-1.))
+
+
+'''
+=========================
+BRINCANDO COM WRAP FUNCTIONS
+========================
+ '''
+
+def fexterna(func):
+
+ 
+   def finterna(*args, **kwargs):
+       print("Chamando funcao: %s()"  % (func.__name__))
+       result = func(*args, **kwargs) + 324.12
+       return result
+ 
+   return finterna
+ 
+def dobro(x):
+    """ Uma funcao exemplo qualquer.
+    """
+    return 2*x
+ 
+dobro_com_print = fexterna(dobro)
+print(dobro_com_print(10))
+
+
+'''ou simplesmente faça com uso de decorador (Python traz essa facilidade)'''
+# @fexterna
+# def dobro(x):
+#     """ Uma funcao exemplo qualquer.
+#     """
+#     return 2*x
+'''para simplesmente chamar:'''
+# print(dobro(5))
+
+'''
+=====================
+USANDO SYMPY
+=================
+'''
+from sympy import Function
+from sympy import lambdify, Matrix
+from sympy.utilities.lambdify import implemented_function
+# from sympy import sqrt, sin, Matrix
+from sympy.abc import w, x, y, z
+
+g = lambdify(y, y**3)
+print(g(2))
+
+'''
+=====================
+USANDO DERIVADAS COM NUMPY
+=================
+'''
+p = np.poly1d([1, 0, 1])
+print(p)
+q = p.deriv()
+print(q)
 
